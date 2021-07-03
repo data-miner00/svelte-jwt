206580a42e8d22ea1aeaa861b140f725c6301dbe
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-03 23:09:13 +0800

    chore: Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Router from 'svelte-spa-router';
+	import Home from './views/Home.svelte'
+	export let name;
+</script>
+
+<div id="app">
+	<Router 
+		routes={{
+			'/': Home,
+		}}
+	/>
+</div>
+
+<style>
+	#app {
+		min-height: 100vh;
+		background: palegoldenrod;
+	}
+</style>
