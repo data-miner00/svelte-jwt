fc6cb98768d2c529f66ad8cf59206ce74d67096d
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-05 17:41:22 +0800

    feat: Added localstorage upon succesful login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-const api = "http://localhost:3000";
+const api = process.env.EXPRESS_APP_URL;
 
 export async function _login({ username, password }) {
   try {
     const res = await axios.post(`${api}/login`, { username, password });
-    return Promise.resolve({ accessToken: res.data.accessToken });
+    const accessToken = res.data.accessToken;
+
+    localStorage.setItem("accessToken", accessToken);
+    return Promise.resolve({ accessToken });
   } catch (err) {
     return Promise.reject(err);
   }
