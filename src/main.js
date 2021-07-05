344d54b6e00b552670390e9e8ab44638ba111272
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-05 17:40:52 +0800

    chore: Config svelte to support env

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: "world",
+  },
 });
 
 export default app;
