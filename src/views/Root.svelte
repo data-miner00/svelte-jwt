1cfb2a113271abfc6c5329f4e04d34f5b60078b3
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-05 15:51:52 +0800

    feat: Basic root page component

---
+++ src/views/Root.svelte
@@ -0,0 +1,60 @@
+<div class="root">
+  <div class="root__container">
+    <div class="root__container__welcome">
+      <h1>Welcome to Svelte Login</h1>
+    </div>
+    <div class="root__container__input">
+      <input type="text" placeholder="username" bind:value={username}>
+    </div>
+    <div class="root__container__input">
+      <input type="password" placeholder="password" bind:value={password}>
+    </div>
+    <div class="root__container__input">
+      <button>Login</button>
+    </div>
+    
+    <div class="root__container__register-link">
+      { authenticated }
+    </div>
+  </div>
+</div>
+
+<script>
+  import { authenticated } from "../stores/index"
+
+  let username = '';
+  let password = '';
+
+  function login() {
+    
+  }
+</script>
+
+
+<style>
+.root {
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+}
+.root__container {
+  max-width: 500px;
+  height: 600px;
+  background: #eee;
+  flex-direction: column;
+  padding: 10px;
+}
+.root__container__welcome {
+  text-decoration: underline;
+}
+.root__container__input {
+  padding: 10px 0;
+}
+.root__container__input input,
+.root__container__input button {
+  font-size: 1em;
+  padding: 5px 8px;
+  width: 100%;
+}
+
+</style>
