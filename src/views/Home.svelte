a356511a091433352e054d0b2573f09b25243e34
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 17:02:50 +0800

    refactor: Purge externalized login

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,18 +1,15 @@
+<Header/>
 <div class="home">
   <h1>Home</h1>
-  <button on:click={logout}>Logout</button>
 </div>
 
 <script>
-  import Icon from "svelte-awesome"
-  import { faThumbsUp } from '@fortawesome/free-regular-svg-icons'
-  import { authState } from '../stores'
-
-  function logout() {
-    authState.update(state => state = false)
-  }
+  import Header from "../components/Header.svelte"
 </script>
 
 <style>
+  .home {
+    padding: 20px 20%;
 
+  }
 </style>
