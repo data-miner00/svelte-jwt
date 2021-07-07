9262c8b4dc94db142c55f0d9d535791a6d26186c
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 19:06:31 +0800

    feat: Added profile route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import Root from "../views/Root.svelte";
 import Secured from "../views/Secured.svelte";
 import NotFound from "../views/NotFound.svelte";
+import Profile from "../views/Profile.svelte";
 
 export default {
   "/": Root,
   "/secured": Secured,
+  "/profile": Profile,
   "*": NotFound,
 };
