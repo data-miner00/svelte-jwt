5665352e022e2d12f5fccdb999621084bdf9001f
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 17:29:25 +0800

    feat: Added a secure page

--- src/views/Secured.svelte
+++ src/views/Secured.svelte
@@ -1,10 +1,22 @@
-<div class="secured">
-  <h1>Secured</h1>
-  <p>This is a secured page</p>
-</div>
+{#if authenticated}
+  <div class="secured">
+    <h1>Secured</h1>
+    <p>This is a secured page</p>
+  </div>
+{:else}
+  <Forbidden />
+{/if}
 
 <script>
-  
+  import { authState } from "../stores"
+  import { onDestroy } from "svelte"
+  import Forbidden from "./Forbidden.svelte"
+
+  let authenticated = false;
+
+  const unAuth = authState.subscribe(state => authenticated = state)
+
+  onDestroy(unAuth)
 </script>
 
 <style>
