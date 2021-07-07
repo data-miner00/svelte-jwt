f5049bec571a93a64261a39304f5a62a20f06679
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 09:55:17 +0800

    feat: Added logout button

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,5 +1,18 @@
-<script>
+<div class="home">
+  <h1>Home</h1>
+  <button on:click={logout}>Logout</button>
+</div>
 
+<script>
+  import Icon from "svelte-awesome"
+  import { faThumbsUp } from '@fortawesome/free-regular-svg-icons'
+  import { authState } from '../stores'
+
+  function logout() {
+    authState.update(state => state = false)
+  }
 </script>
 
-<h1>Home</h1>
+<style>
+
+</style>
