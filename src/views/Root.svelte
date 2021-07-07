a55756459618cdd3ca05bcb59827ffc0fb430448
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 09:55:02 +0800

    feat: Proper login loading

--- src/views/Root.svelte
+++ src/views/Root.svelte
@@ -1,37 +1,52 @@
-<div class="root">
-  <div class="root__container">
-    <div class="root__container__welcome">
-      <h1>Welcome to Svelte Login</h1>
-    </div>
-    <div class="root__container__input">
-      <input type="text" placeholder="username" bind:value={username}>
-    </div>
-    <div class="root__container__input">
-      <input type="password" placeholder="password" bind:value={password}>
-    </div>
-    <div class="root__container__input">
-      <button on:click={login}>Login</button>
-    </div>
-    
-    <div class="root__container__register-link">
-      { accessToken }
+{#if !authenticated}
+  <div class="root">
+    <div class="root__container">
+      <div class="root__container__welcome">
+        <h1>Welcome to Svelte Login</h1>
+      </div>
+      <div class="root__container__input">
+        <input type="text" placeholder="username" bind:value={username}>
+      </div>
+      <div class="root__container__input">
+        <input type="password" placeholder="password" bind:value={password}>
+      </div>
+      <div class="root__container__input">
+        <button on:click={login}>Login</button>
+      </div>
+      
+      <div class="root__container__register-link">
+        <span>Sign up for a new account</span>
+      </div>
+
+      <div>{ accessToken }</div>
     </div>
   </div>
-</div>
+{:else}
+  <Home />
+{/if}
 
 <script>
-  import { authenticated } from "../stores/index"
+  import { authState } from "../stores/index"
   import { _login } from '../services/authService'
+  import Home from "./Home.svelte"
+  import { onDestroy } from "svelte"
 
   let username = '';
   let password = '';
   let accessToken = '';
   
+  let authenticated = false;
+
+  const unAuth = authState.subscribe(state => authenticated = state)
+
+  onDestroy(unAuth)
 
   async function login() {
     try {
       const res = await _login({ username, password })
       accessToken = res.accessToken
+  
+      authState.update(value => value = true)
       console.log(accessToken)
     } catch (error) {
       accessToken = "WTF are you trying to do"
