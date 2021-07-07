4b3e32e9f079406ab872a312a9f036f2804508c8
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 17:01:58 +0800

    feat: Added a secured route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import Root from "../views/Root.svelte";
+import Secured from "../views/Secured.svelte";
 import NotFound from "../views/NotFound.svelte";
 
 export default {
   "/": Root,
+  "/secured": Secured,
   "*": NotFound,
 };
