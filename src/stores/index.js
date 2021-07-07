f1855c441070f0d380d9213256087bbb0210664f
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 22:30:43 +0800

    feat: Added 2 more global state

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,4 +1,6 @@
 import { writable } from "svelte/store";
 
 // export const user = writable({})
-export const authState = writable(false);
+export const authState = writable(true);
+export const accessToken = writable("");
+export const userInfo = writable({ username: "", id: 0 });
