83ec1906b21e7e2abe07b2ef707541cde04f16a8
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 21:40:52 +0800

    feat: Polished login page

--- src/views/Root.svelte
+++ src/views/Root.svelte
@@ -1,35 +1,39 @@
 {#if !authenticated}
   <div class="root">
+    <div class="root__illustration"></div>
     <div class="root__container">
-      <div class="root__container__welcome">
-        <h1>Welcome to Svelte Login</h1>
-      </div>
-      <form on:submit|preventDefault={login}>
-        <div class="root__container__input">
-          <input type="text" placeholder="username" bind:value={username}>
+      <div>
+        <div class="root__container__logo">
+          <img src="/svelte.png" alt="">
         </div>
-        <div class="root__container__input">
-          <input type="password" placeholder="password" bind:value={password}>
+        <div class="root__container__login-label">
+          <h1>Sign In</h1>
         </div>
-        <div class="root__container__input">
-          <button>Login</button>
-        </div>
-      </form>
+        <form class="root__container__form" on:submit|preventDefault={login}>
+          <div class="root__container__input">
+            <label for="username">Username</label>
+            <input type="text" id="username" bind:value={username}>
+          </div>
+          <div class="root__container__input">
+            <label for="password">Password</label>
+            <input type="password" id="password" bind:value={password}>
+          </div>
 
-      {#if errorMessage}
-        <div class="root__container__error">
-          <span>{errorMessage}</span>
+          {#if errorMessage}
+            <div class="root__container__error">
+              <span>{errorMessage}</span>
+            </div>
+          {/if}
+
+          <div class="root__container__login-btn">
+            <button>Login</button>
+          </div>
+        </form>
+        <div class="root__container__register-link">
+          <span>Sign up for a new account</span>
         </div>
-      {/if}
+      </div>
       
-      <div class="root__container__register-link">
-        <span>Sign up for a new account</span>
-      </div>
-
-      
-      <div class="root__container__sample-navs">
-
-      </div>
     </div>
   </div>
 {:else}
@@ -98,32 +102,91 @@
 
 <style>
   .root {
-    justify-content: center;
-    align-items: center;
     flex-grow: 1;
+    
+    height: 100vh;
+    display: flex;
+  }
+  .root__illustration {
+    flex: 1 1 0px;
+    background-image: url("/landing2.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
   }
   .root__container {
-    max-width: 500px;
-    height: 600px;
-    background: #eee;
-    flex-direction: column;
-    padding: 10px;
+    flex: 1 1 0px;
+    background: white;
+    /* box-shadow: 2px 2px 5px 2px gray; */
+    display: flex;
+    padding: 50px 80px;
+    justify-content: center;
+    align-items: center;
   }
-  .root__container__welcome {
-    text-decoration: underline;
+  .root__container div {
+    max-width: 500px;
+  }
+  .root__container__logo {
+    margin-bottom: 100px;
+  }
+  .root__container__logo img {
+    width: 80px;
+  }
+  .root__container__form {
+    margin-top: 50px;
+    
+  }
+  .root__container__login-label {
+    font-size: 24px;
   }
   .root__container__input {
     padding: 10px 0;
+    position: relative;
+    margin-top: 10px;
   }
-  .root__container__input input,
-  .root__container__input button {
-    font-size: 1em;
-    padding: 5px 8px;
+  .root__container__input::after {
+    position: absolute;
+    content: "";
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 3px;
+    background: #ff451c;
+  }
+  .root__container__input label {
+    font-size: 20px;
+    font-weight: 700;
+  }
+  .root__container__input input {
+    border: none;
+    font-size: 1.3em;
     width: 100%;
+    /* padding: 8px 0; */
+    outline: none;
+    color: rgb(92, 87, 87);
   }
-
+  .root__container__login-btn {
+    margin-top: 50px;
+  }
+  .root__container__login-btn button {
+    font-size: 1em;
+    padding: 8px 12px;
+    width: 100%;
+    border: none;
+    background: #ff451c;
+    border-radius: 5px;
+    font-size: 20px;
+    font-weight: 700;
+    color: white;
+  }
   .root__container__error {
     font-weight: 700;
     color: crimson;
+  }
+  .root__container__register-link {
+    color: deepskyblue;
+    margin-top: 10px;
+    text-align: center;
   }
 </style>
