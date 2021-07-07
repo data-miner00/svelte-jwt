3abaf2c3e4f1e0b1b7cf1caa545bc199093ad31c
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-07 23:28:49 +0800

    feat: Profile page with successful data fetch

---
+++ src/views/Profile.svelte
@@ -0,0 +1,35 @@
+<div class="profile">
+
+</div>
+
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  import { getProfile } from "../services/apiService"
+  import { userInfo } from "../stores"
+
+  let profile = {
+    id: -1,
+    username: "",
+    phoneNumber: "",
+    email: "",
+    age: -1,
+  };
+  let userId;
+
+  const unUser = userInfo.subscribe(i => userId = i.id)
+
+  onMount(async () => {
+    try {
+      profile = await getProfile(userId)
+      console.log(profile);
+    } catch (err) {
+      console.error(err);
+    }
+  })
+
+  onDestroy(unUser)
+</script>
+
+<style>
+
+</style>
